5b5119938df5d587888b5d3b7757e850e977a956
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-03-22 11:02:17 -0300

    Added service module

---
+++ modules/ecs-service/variables.tf
@@ -0,0 +1,24 @@
+variable "service_name" {
+  type        = string
+  description = "Name of the Service"
+}
+
+variable "cluster_id" {
+  type        = string
+  description = "The cluster that the service will be created in"
+}
+
+variable "task_name" {
+  type        = string
+  description = "The name of the task definition"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "The subnets of the service"
+}
