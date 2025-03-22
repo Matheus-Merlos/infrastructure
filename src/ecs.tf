9238284d80d9fcf9b6fe9fe4318a4da4a25bcab2
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-03-22 10:54:54 -0300

    Added ecs cluster base

--- src/ecs.tf
+++ src/ecs.tf
@@ -1,3 +1,30 @@
 resource "aws_ecs_cluster" "tf_services" {
   name = "tf-services"
 }
+
+data "aws_iam_policy_document" "ecs_task_execution_policy_document" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_policy_document.json
+}
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs-task-role"
+
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
