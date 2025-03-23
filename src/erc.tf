5cf09fc471f9eb8ce048524a807e3e4e5d6798c3
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-03-23 00:03:26 -0300

    Removed ECS cluster configurations for now, and added iam.tf

--- src/erc.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecr_repository" "projects_repository" {
-  name                 = "projects"
-  image_tag_mutability = "MUTABLE"
-}
--- src/iam.tf
+++ src/iam.tf
@@ -1,57 +1,81 @@
-resource "aws_iam_user" "ecr_user" {
-  name = "ecr_user"
+/*
+User to run terraform in GitHub Actions
+*/
+resource "aws_iam_user" "dns_checker" {
+  name = "dns-checker"
 
   lifecycle {
     ignore_changes = [ tags ]
   }
 }
 
-data "aws_iam_policy_document" "ecs_user_policy" {
+data "aws_iam_policy_document" "dns_checker_policy" {
   statement {
     effect = "Allow"
     actions = [
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:CompleteLayerUpload",
-      "ecr:InitiateLayerUpload",
-      "ecr:PutImage",
-      "ecr:BatchGetImage",
-      "ecr:UploadLayerPart",
-      "ecr:CreateRepository",
-      "ecr:DescribeRepositories",
-      "ecs:RegisterTaskDefinition",
-      "ecs:DescribeTaskDefinition",
-      "ecs:ListTaskDefinitions",
-      "elasticfilesystem:DescribeFileSystems",
-      "elasticfilesystem:DescribeAccessPoints"
+      "s3:ListBucket",
+      "s3:PutObject",
+      "s3:Get*"
     ]
     resources = [
-      "*"
+      "arn:aws:s3:::tf-neon-bot-terraform-remote-state",
+      "arn:aws:s3:::tf-neon-bot-terraform-remote-state/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:Describe*"
+    ]
+    resources = [ "*" ]
+  }
+}
+
+resource "aws_iam_policy" "dns_checker_policy" {
+  name   = "DNSChecker"
+  policy = data.aws_iam_policy_document.dns_checker_policy.json
+}
+
+resource "aws_iam_user_policy_attachment" "dns_checker_attachment" {
+  user       = aws_iam_user.dns_checker.name
+  policy_arn = aws_iam_policy.dns_checker_policy.arn
+}
+
+
+
+
+
+resource "aws_iam_user" "image_upload" {
+  name = "image-upload"
+
+  lifecycle {
+    ignore_changes = [ tags ]
+  }
+}
+
+data "aws_iam_policy_document" "image_upload_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:PutObjectAcl",
+      "s3:DeleteObject"
+    ]
+    resources = [
+      "arn:aws:s3:::neon-bot-images",
+      "arn:aws:s3:::neon-bot-images/*"
     ]
   }
 }
 
-resource "aws_iam_policy" "ecs_user_policy" {
-  name   = "ECSImageUserPolicy"
-  policy = data.aws_iam_policy_document.ecs_user_policy.json
+resource "aws_iam_policy" "image_upload_policy" {
+  name   = "S3ImageBucketUpload"
+  policy = data.aws_iam_policy_document.image_upload_policy.json
 }
 
-resource "aws_iam_user_policy_attachment" "ecr_user_policy_attachment" {
-  user       = aws_iam_user.ecr_user.name
-  policy_arn = aws_iam_policy.ecs_user_policy.arn
-}
-
-resource "aws_iam_access_key" "ecr_access_key" {
-  user = aws_iam_user.ecr_user.name
-}
-
-
-
-output "ecr_user_access_key_id" {
-  value = aws_iam_access_key.ecr_access_key.id
-}
-
-output "ecr_user_secret_access_key" {
-  value     = aws_iam_access_key.ecr_access_key.secret
-  sensitive = true
+resource "aws_iam_user_policy_attachment" "image_upload_policy_attachment" {
+  user       = aws_iam_user.image_upload.name
+  policy_arn = aws_iam_policy.image_upload_policy.arn
 }
