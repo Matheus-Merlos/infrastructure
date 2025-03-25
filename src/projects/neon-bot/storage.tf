fdbf3ab049111bebeca5547d5b5b22bb50d8d8e0
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-03-24 22:37:14 -0300

    feat(neon-bot-infrastructure): Changed DynamoDB table for neon bot

--- src/projects/neon-bot/storage.tf
+++ src/projects/neon-bot/storage.tf
@@ -2,12 +2,12 @@ resource "aws_dynamodb_table" "neon_bot_table_players" {
   name         = "neon-bot-players"
   billing_mode = "PAY_PER_REQUEST"
 
-  hash_key  = "id"
+  hash_key  = "playerId"
   range_key = "guildId"
 
   attribute {
-    name = "id"
-    type = "N"
+    name = "playerId"
+    type = "S"
   }
 
   attribute {
