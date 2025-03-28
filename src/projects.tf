c017f247f9df76d3bb7d198898259603fda31ba9
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-03-28 19:08:34 -0300

    feat(iam): Removed DB IAM User

--- src/projects.tf
+++ src/projects.tf
@@ -5,16 +5,6 @@ module "neon-bot" {
   private_subnet_ids = local.public_subnets
 }
 
-output "db_user_access_key_id" {
-  value     = module.neon-bot.db_user_access_key_id
-  sensitive = true
-}
-
-output "db_user_secret_access_key" {
-  value     = module.neon-bot.db_user_secret_access_key
-  sensitive = true
-}
-
 output "neon_bot_db_password" {
   value     = module.neon-bot.db_password
   sensitive = true
--- src/projects/neon-bot/outputs.tf
+++ src/projects/neon-bot/outputs.tf
@@ -1,13 +1,3 @@
-output "db_user_access_key_id" {
-  value     = aws_iam_access_key.db_user_access_key.id
-  sensitive = true
-}
-
-output "db_user_secret_access_key" {
-  value     = aws_iam_access_key.db_user_access_key.secret
-  sensitive = true
-}
-
 output "db_password" {
   value     = random_string.postgresql_password.result
   sensitive = true
